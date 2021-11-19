459ab390551790b7b16a146169a59c484156b11c
thisaraMJ <madushantjg@gmail.com>
2021-11-19 23:27:11 +0530

    initial commit

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM openjdk:11
-EXPOSE 8080
-ADD build/petstore-runner.jar petstore.jar
-ENTRYPOINT ["java","-jar","petstore.jar"]
---
+++ src/main/resources/data-postgres.sql
@@ -0,0 +1,9 @@
+INSERT INTO Pet
+VALUES  (1,'Cat','kitty',3),
+        (2,'Dog','Blacky',3),
+        (3,'Pig','Fatty',5);
+
+INSERT INTO PetType
+VALUES  (1,'Cat'),
+        (2,'Dog'),
+        (3,'Pig');
